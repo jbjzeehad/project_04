ac6102747c5f6ec0eb48d6a04749bdd20bac9ac1
jbjzeehad <ilamta.97@gmail.com>
2023-10-04 18:05:24 +0600

    cardentry

--- calculation.js
+++ calculation.js
@@ -1,31 +1,5 @@
 console.log("ok i am done");
 
-document.getElementById('card-01').addEventListener('click', function () {
-    console.log('Card 01 clicked');
-
-
-})
-document.getElementById('card-02').addEventListener('click', function () {
-    console.log('Card 02 clicked');
-})
-document.getElementById('card-03').addEventListener('click', function () {
-    console.log('Card 03 clicked');
-})
-document.getElementById('card-04').addEventListener('click', function () {
-    console.log('Card 04 clicked');
-})
-document.getElementById('card-05').addEventListener('click', function () {
-    console.log('Card 05 clicked');
-})
-document.getElementById('card-06').addEventListener('click', function () {
-    console.log('Card 06 clicked');
-})
-document.getElementById('card-07').addEventListener('click', function () {
-    console.log('Card 07 clicked');
-})
-document.getElementById('card-08').addEventListener('click', function () {
-    console.log('Card 08 clicked');
-})
 // document.getElementById('card-09').addEventListener('click', function () {
 //     console.log('Card 09 clicked');
 //     const card09Pricestring = document.getElementById('c9price');
@@ -38,10 +12,26 @@ document.getElementById('card-08').addEventListener('click', function () {
 
 function clickedCard(inputvalue) {
     console.log(inputvalue, " Function called");
-    const card09Pricestring = document.getElementById(inputvalue);
-    const card09Price = parseFloat(card09Pricestring.innerText);
-    console.log(card09Price.toFixed(2));
-    console.log(typeof (card09Price));
+    const cardinPricestring = document.getElementById(inputvalue);
+    const cardinPrice = parseFloat(cardinPricestring.innerText);
+    console.log(cardinPrice.toFixed(2));
+    console.log(typeof (cardinPrice));
     const totalPricestring = document.getElementById('total-price');
-    totalPricestring.innerText = card09Price.toFixed(2);
+    totalPricestring.innerText = cardinPrice.toFixed(2);
+
+    const totalstring = document.getElementById('total');
+    totalstring.innerText = cardinPrice.toFixed(2);
+}
+
+
+function cardNameEntry(cardname) {
+    console.log("working", cardname);
+    const cname = document.getElementById(cardname);
+    const cnamestring = cname.innerText;
+    const cardentrystring = document.getElementById('card-entry');
+    const count = cardentrystring.childElementCount;
+    const par = document.createElement('p');
+    par.innerHTML = `${count + 1}. ${cnamestring}`;
+    cardentrystring.appendChild(par);
+
 }
