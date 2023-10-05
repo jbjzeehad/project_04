18588c00758d069c750d808723bec9b1ec267228
jbjzeehad <ilamta.97@gmail.com>
2023-10-05 20:32:12 +0600

    Completed

--- calculation.js
+++ calculation.js
@@ -1,6 +1,7 @@
 
+// Discount Calculation and final price
+
 document.getElementById('dis-btn').addEventListener('click', function () {
-    console.log("running");
     const discode = document.getElementById('discount-code');
     if (discode.value === 'SELL200') {
         const currenttotalstring = document.getElementById('total-price');
@@ -15,30 +16,42 @@ document.getElementById('dis-btn').addEventListener('click', function () {
     }
 })
 
+// Clicked card, total and final price
 
 function clickedCard(inputvalue) {
+
+    // Enable purchase button
+
     const makePurchasebutton = document.getElementById('purchase-btn');
     makePurchasebutton.removeAttribute('disabled');
 
     const cardinPricestring = document.getElementById(inputvalue);
     const cardinPrice = parseFloat(cardinPricestring.innerText);
+
+    // Total price
+
     const totalPricestring = document.getElementById('total-price');
     const totalPricevalue = parseFloat(totalPricestring.innerText);
     const totalitemprice = totalPricevalue + cardinPrice;
     totalPricestring.innerText = totalitemprice.toFixed(2);
 
-    if (totalitemprice >= 200) {
-        console.log('Bonus button open');
+    // Enable discount button
 
+    if (totalitemprice >= 200) {
         const bonusbutton = document.getElementById('dis-btn');
         bonusbutton.removeAttribute('disabled');
     }
+
+    // Final price
+
     const totalstring = document.getElementById('total');
     totalstring.innerText = totalitemprice.toFixed(2);
 }
 
+// Product name entry
+
 function cardNameEntry(cardname) {
-    console.log("working", cardname);
+
     const cname = document.getElementById(cardname);
     const cnamestring = cname.innerText;
     const cardentrystring = document.getElementById('card-entry');
@@ -49,8 +62,34 @@ function cardNameEntry(cardname) {
 
 }
 
+// Promo code button if clicked
+
 function bonusButton(couponcode) {
-    console.log(couponcode);
+
     const inputstring = document.getElementById('discount-code');
     inputstring.value = couponcode;
 }
+
+// Modal button clicked
+
+// Cleared all the value,item name and set default
+
+document.getElementById('home-btn').addEventListener('click', function () {
+
+    const discode = document.getElementById('discount-code');
+    discode.value = "";
+    const cardentrystring = document.getElementById('card-entry');
+    cardentrystring.innerHTML = "";
+
+    const totalstring = document.getElementById('total');
+    totalstring.innerText = "00.00";
+
+    const totalPricestring = document.getElementById('total-price');
+    totalPricestring.innerText = "00.00";
+
+    const disstring = document.getElementById('discount');
+    disstring.innerText = "00.00";
+
+    const modal = document.getElementById('mymodal');
+    modal.style.display = "none";
+})
